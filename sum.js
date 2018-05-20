cac7ef204095c33969f6a1ba7353d97678e8ed81
github username <dsbondin@gmail.com>
2018-05-20 19:52:01 -0400

    add comment

--- sum.js
+++ sum.js
@@ -3,6 +3,7 @@ let sum = process.argv
   .reduce((prev, next) => parseInt(prev) + parseInt(next))
 
 console.log(sum)
+// process.stdout.write(sum + '\n') same as console.log(sum)
 
 // ♥ node sum.js 3 5 4
 // 12
